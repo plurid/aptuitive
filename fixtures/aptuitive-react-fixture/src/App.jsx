526bbfd1f0d4c23f28ecace9b7d44712ef6346c9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-28 17:41:03 +0200

    aptuitive-react > fixture: App

--- fixtures/aptuitive-react-fixture/src/App.jsx
+++ fixtures/aptuitive-react-fixture/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import Aptuitive from '@plurid/aptuitive-react';
+
+
+
+const Button = () => {
+    return (
+        <button>
+            button
+        </button>
+    );
+}
+
+const aptuitive = new Aptuitive();
+
+const AptButton = aptuitive.add(Button);
 
 
 const App = () => {
     return (
         <div>
+            <AptButton />
         </div>
     );
 }
